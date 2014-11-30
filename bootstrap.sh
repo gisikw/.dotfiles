0b1e1741153b073ba3f509b1efa091490a0ba5b7
Kevin Gisi <kevin@kevingisi.com>
2014-11-30 00:34:29 -0500

    Basic dependency checking

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,20 @@
-git ls-remote https://github.com/gisikw/dotfiles.git master | sed -e 's/[	,\s].*$//'
+#!/bin/bash
+
+which sed > /dev/null || { echo "sed is not installed. Aborting."; exit 1; }
+which git > /dev/null || { echo "git is not installed. Aborting."; exit 1; }
+
+if [ ! -d ~/.dotfiles ]; then
+  echo "The git repo hasn't been cloned. We would do so here."
+  exit 1
+fi
+
+LATEST=$(git ls-remote --heads https://github.com/gisikw/dotfiles.git master | sed -e 's/[	,\s].*$//')
+CURRENT=$(git ls-remote --heads ~/.dotfiles master | sed -e 's/[	,\s].*$//')
+
+if [ "$CURRENT" = "$LATEST" ]; then
+  echo "Congrats, you are up-to-date!"
+else
+  echo "Current repo SHA: $CURRENT"
+  echo "Latest repo SHA: $LATEST"
+  echo "Here we begin logic to update things"
+fi
