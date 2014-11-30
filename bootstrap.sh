8a0979262485fc1f11186c462c865173e7a77e63
Kevin Gisi <kevin@kevingisi.com>
2014-11-30 00:43:25 -0500

    Implement git updating

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,18 +3,15 @@
 which sed > /dev/null || { echo "sed is not installed. Aborting."; exit 1; }
 which git > /dev/null || { echo "git is not installed. Aborting."; exit 1; }
 
-if [ ! -d ~/.dotfiles ]; then
-  echo "The git repo hasn't been cloned. We would do so here."
-  exit 1
-fi
-
-LATEST=$(git ls-remote --heads https://github.com/gisikw/dotfiles.git master | sed -e 's/[	,\s].*$//')
-CURRENT=$(git ls-remote --heads ~/.dotfiles master | sed -e 's/[	,\s].*$//')
-
-if [ "$CURRENT" = "$LATEST" ]; then
-  echo "Congrats, you are up-to-date!"
+if [ ! -d ~/.dotfiles/.git ]; then
+  git clone https://github.com/gisikw/dotfiles.git ~/.dotfiles && echo "Congratulations, the repo has been installed"
 else
-  echo "Current repo SHA: $CURRENT"
-  echo "Latest repo SHA: $LATEST"
-  echo "Here we begin logic to update things"
+  LATEST=$(git ls-remote --heads https://github.com/gisikw/dotfiles.git master | sed -e 's/[	,\s].*$//')
+  CURRENT=$(git ls-remote --heads ~/.dotfiles master | sed -e 's/[	,\s].*$//')
+
+  if [ "$CURRENT" = "$LATEST" ]; then
+    echo "Congrats, you are up-to-date!"
+  else
+    cd ~/.dotfiles && git pull origin master && echo "Congrats, you are up-to-date!"
+  fi
 fi
