498ac0e6ea010fdfc746e4db5746b49b2407d145
Kevin Gisi <kevin@kevingisi.com>
2016-01-20 05:05:14 -0500

    Automatic update

--- bash/bash_prompt.sh
+++ bash/bash_prompt.sh
@@ -1,39 +1,10 @@
-## Colors?  Used for the prompt.
-#Regular text color
-BLACK='\[\e[0;30m\]'
-#Bold text color
-BBLACK='\[\e[1;30m\]'
-#background color
-BGBLACK='\[\e[40m\]'
-RED='\[\e[0;31m\]'
-BRED='\[\e[1;31m\]'
-BGRED='\[\e[41m\]'
-GREEN='\[\e[0;32m\]'
-BGREEN='\[\e[1;32m\]'
-BGGREEN='\[\e[1;32m\]'
-YELLOW='\[\e[0;33m\]'
-BYELLOW='\[\e[1;33m\]'
-BGYELLOW='\[\e[1;33m\]'
-BLUE='\[\e[0;34m\]'
-BBLUE='\[\e[1;34m\]'
-BGBLUE='\[\e[1;34m\]'
-MAGENTA='\[\e[0;35m\]'
-BMAGENTA='\[\e[1;35m\]'
-BGMAGENTA='\[\e[1;35m\]'
-CYAN='\[\e[0;36m\]'
-BCYAN='\[\e[1;36m\]'
-BGCYAN='\[\e[1;36m\]'
-WHITE='\[\e[0;37m\]'
-BWHITE='\[\e[1;37m\]'
-BGWHITE='\[\e[1;37m\]'
+PROMPT_COMMAND=status_prompt
 
-PROMPT_COMMAND=smile_prompt
-
-function smile_prompt
+function status_prompt
 {
   if [ "$?" -eq "0" ]; then
-    PS1="${GREEN}$ "
+    PS1="\[\e[0;32m\]$\[\e[0m\] "
   else
-    PS1="${RED}$ "
+    PS1="\[\e[0;31m\]$\[\e[0m\] "
   fi
 }
