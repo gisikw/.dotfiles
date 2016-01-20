14aa04184ff4d2265f0ff32db65709ace66865a8
Kevin Gisi <kevin@kevingisi.com>
2016-01-20 06:42:40 -0500

    Automatic update

--- bash/utils.sh
+++ bash/utils.sh
@@ -1,3 +1,7 @@
 function gh() {
-  open https://github.com/$1
+  if [ -z $1 ]; then
+    open https://github.com/$(git remote show origin | grep "Fetch URL" | sed 's/.*github.com:\(.*\)\.git/\1/')
+  else
+    open https://github.com/$1
+  fi
 }
