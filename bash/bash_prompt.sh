34944992a7c77ebf42e5fe0d2160439b536d9dab
Kevin Gisi <kevin@kevingisi.com>
2016-01-20 05:23:28 -0500

    Automatic update

--- bash/bash_prompt.sh
+++ bash/bash_prompt.sh
@@ -5,12 +5,12 @@ function status_prompt
   if [ "$?" -ne "0" ]; then
     PS1="\[\e[0;31m\]$\[\e[0m\] "
   else
-    if [ $(git status; echo "$?") == "0" ] || [ -z "$(git status --porcelain)" ]; then
+    if [ $(git status 2>/dev/null; echo "$?") == "0" ] || [ -z "$(git status --porcelain)" ]; then
       echo "Not a git repo, or clean git repo"
-      PS1="\[\e[0;33m\]$\[\e[0m\] "
+      PS1="\[\e[0;32m\]$\[\e[0m\] "
     else
       echo "messy git repo"
-      PS1="\[\e[0;32m\]$\[\e[0m\] "
+      PS1="\[\e[0;33m\]$\[\e[0m\] "
     fi
   fi
 }
