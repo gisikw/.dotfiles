460c9d99fc5938f6cd0890770712a5cbf277b309
Kevin Gisi <kevin@kevingisi.com>
2016-01-20 05:22:10 -0500

    Automatic update

--- bash/bash_prompt.sh
+++ bash/bash_prompt.sh
@@ -5,10 +5,10 @@ function status_prompt
   if [ "$?" -ne "0" ]; then
     PS1="\[\e[0;31m\]$\[\e[0m\] "
   else
-    if [ $(git status; echo "$?") == "0" ] || [ ! test -z "$(git status --porcelain)" ]; then
-      PS1="\[\e[0;32m\]$\[\e[0m\] "
-    else
+    if [ $(git status; echo "$?") == "0" ] || [ -z "$(git status --porcelain)" ]; then
       PS1="\[\e[0;33m\]$\[\e[0m\] "
+    else
+      PS1="\[\e[0;32m\]$\[\e[0m\] "
     fi
   fi
 }
