22a47bb821f62ea63acdc03adf5de8fdb78effe9
Kevin Gisi <kevin@kevingisi.com>
2016-05-07 20:55:58 -0400

    Automatic update

--- bash/bash_prompt.sh
+++ bash/bash_prompt.sh
@@ -2,18 +2,23 @@ PROMPT_COMMAND=status_prompt
 
 function status_prompt() {
   if [ "$?" -ne "0" ]; then
-    PS1="\[\e[0;31m\]$\[\e[0m\] "
+    PROMPT="\[\e[0;31m\]$\[\e[0m\] "
   else
     # Skip git test on mounted volumes
     if [[ "$(pwd)" == "/Volumes/"* ]]; then
-      PS1="\[\e[0;32m\]$\[\e[0m\] "
+      PROMPT="\[\e[0;32m\]$\[\e[0m\] "
     else
       gstatus="$(git status --porcelain 2>/dev/null | wc -c)"
       if [ "$?" -ne "0" ] || [ "$gstatus" -eq "0" ]; then
-        PS1="\[\e[0;32m\]$\[\e[0m\] "
+        PROMPT="\[\e[0;32m\]$\[\e[0m\] "
       else
-        PS1="\[\e[0;33m\]$\[\e[0m\] "
+        PROMPT="\[\e[0;33m\]$\[\e[0m\] "
       fi
     fi
+  fi
+  if [ -n "$SSH_CLIENT" ]; then
+    PS1=*$PROMPT
+  else
+    PS1=$PROMPT
   fi
 }
