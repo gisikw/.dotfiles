11291afb48d1edca1f3ba9bbd6988c48fb346f09
Kevin Gisi <kevin@kevingisi.com>
2016-09-14 03:10:55 -0500

    Automatic update

---
+++ bash/lint.sh
@@ -0,0 +1,9 @@
+function lint() {
+  # TODO: Use filewatcher '**/*.rb' and '**/*.js' and... etc, 'lint_file $FILENAME'
+}
+
+function lint_file() {
+  if [[ $1 == *.rb ]]; then
+    rubocop $1
+  fi
+}
