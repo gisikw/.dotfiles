cb298fe9b8fb77a932b002e6deabe8f1b73e7eb0
Kevin Gisi <kevin@kevingisi.com>
2016-09-14 03:22:08 -0500

    Automatic update

--- bash/lint.sh
+++ bash/lint.sh
@@ -4,5 +4,5 @@ function lint() {
 }
 
 function lint_file() {
-  ext=${1##*\.}; case "$ext" in; rb) rubocop $1;; js) eslint $1;; scss) scss-lint $1;;esac
+  ext=${1##*\.}; case "$ext" in rb) rubocop $1;; js) eslint $1;; scss) scss-lint $1;;esac
 }
