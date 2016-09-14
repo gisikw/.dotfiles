182aa0fa8738ab827789ef4cfd2b0aafe59da074
Kevin Gisi <kevin@kevingisi.com>
2016-09-14 03:24:20 -0500

    Automatic update

--- bash/lint.sh
+++ bash/lint.sh
@@ -1,3 +1,3 @@
 function lint() {
-  filewatcher -s -r '**/*' 'ext=${1##*\.}; case "$ext" in rb) rubocop $1;; js) eslint $1;; scss) scss-lint $1;;esac'
+  filewatcher -s -r '**/*.rb **/*.js **/*scss' 'ext=${1##*\.}; case "$ext" in rb) rubocop $1;; js) eslint $1;; scss) scss-lint $1;;esac'
 }
