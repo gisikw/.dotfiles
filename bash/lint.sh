6bf96113b146f167434955247eeb22c42ae00280
Kevin Gisi <kevin@kevingisi.com>
2016-09-14 03:13:48 -0500

    Automatic update

--- bash/lint.sh
+++ bash/lint.sh
@@ -1,6 +1,6 @@
-function lint() {
-  # TODO: Use filewatcher '**/*.rb' and '**/*.js' and... etc, 'lint_file $FILENAME'
-}
+# function lint() {
+#   # TODO: Use filewatcher '**/*.rb' and '**/*.js' and... etc, 'lint_file $FILENAME'
+# }
 
 function lint_file() {
   if [[ $1 == *.rb ]]; then
