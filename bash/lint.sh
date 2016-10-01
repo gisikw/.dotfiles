f70e4010a5d814fa0b2a32d101f8a2361a841e24
Kevin Gisi <kevin@kevingisi.com>
2016-09-30 22:04:20 -0500

    Automatic update

--- bash/lint.sh
+++ bash/lint.sh
@@ -1,7 +1,9 @@
 function lint() {
-  filewatcher -s -r '**/*.rb **/*.js **/*scss' 'echo $FILENAME; ext=${FILENAME##*\.}; case "$ext" in rb) rubocop $FILENAME;; js) eslint $FILENAME;; scss) scss-lint $FILENAME;;esac'
-}
 
-function lintgo() {
-  filewatcher -r '**/*.go' 'out=$(golint $FILENAME); if [ -z "$out" ]; then echo "\\033[0;32mAll Good\\033[0m"; else echo "\\033[0;31m$out\\033[0m"; fi'
+  case "$1" in
+    go)
+      filewatcher -r '**/*.go' 'out=$(golint $FILENAME); if [ -z "$out" ]; then echo "\\033[0;32mAll Good\\033[0m"; else echo "\\033[0;31m$out\\033[0m"; fi';;
+  esac
+
+  # filewatcher -s -r '**/*.rb **/*.js **/*scss' 'echo $FILENAME; ext=${FILENAME##*\.}; case "$ext" in rb) rubocop $FILENAME;; js) eslint $FILENAME;; scss) scss-lint $FILENAME;;esac'
 }
