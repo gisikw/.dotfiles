c34222cbba759debfb7e27b20c24da8ba9ca561c
Kevin Gisi <kevin@kevingisi.com>
2016-11-27 12:56:31 -0600

    Automatic update

--- bash/ssh_agent.sh
+++ bash/ssh_agent.sh
@@ -15,8 +15,8 @@ if [ -f "${SSH_ENV}" ]; then
     . "${SSH_ENV}" > /dev/null
     #ps ${SSH_AGENT_PID} doesn't work under cywgin
     ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
-        start_agent;
+        start_agent && clear;
     }
 else
-    start_agent;
+    start_agent && clear;
 fi
