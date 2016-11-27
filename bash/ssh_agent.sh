eb0d96d3490ba4ca6adde1f1e734d0f01b34940a
Kevin Gisi <kevin@kevingisi.com>
2016-11-27 12:58:53 -0600

    Automatic update

--- bash/ssh_agent.sh
+++ bash/ssh_agent.sh
@@ -1,7 +1,7 @@
 SSH_ENV="$HOME/.ssh/environment"
 
 function start_agent {
-    /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}" 2>/dev/null
+    /usr/bin/ssh-agent 2>/dev/null | sed 's/^echo/#echo/' > "${SSH_ENV}"
     chmod 600 "${SSH_ENV}"
     . "${SSH_ENV}" > /dev/null
     /usr/bin/ssh-add;
