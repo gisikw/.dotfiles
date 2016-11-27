578d0f73a6a03d1984e34b7d2a4b39572254c617
Kevin Gisi <kevin@kevingisi.com>
2016-11-27 12:57:19 -0600

    Automatic update

--- bash/ssh_agent.sh
+++ bash/ssh_agent.sh
@@ -1,9 +1,7 @@
 SSH_ENV="$HOME/.ssh/environment"
 
 function start_agent {
-    echo "Initialising new SSH agent..."
     /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
-    echo succeeded
     chmod 600 "${SSH_ENV}"
     . "${SSH_ENV}" > /dev/null
     /usr/bin/ssh-add;
@@ -15,8 +13,8 @@ if [ -f "${SSH_ENV}" ]; then
     . "${SSH_ENV}" > /dev/null
     #ps ${SSH_AGENT_PID} doesn't work under cywgin
     ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
-        start_agent && clear;
+        start_agent;
     }
 else
-    start_agent && clear;
+    start_agent;
 fi
