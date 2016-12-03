b9d5b1dcd5e81a7306504de6845c38127d4d87e5
Kevin Gisi <kevin@kevingisi.com>
2016-12-03 15:03:30 -0600

    Automatic update

--- bash/mouse.sh
+++ bash/mouse.sh
@@ -1,6 +1,6 @@
 # Set mouse acceleration and disable secondary keys for Logitech Marble Mouse
-which xset >/dev/null && xset m 3/2 0
-MOUSE=$(xinput list | grep "Logitech USB Trackball" | sed 's/.*id=\([[:digit:]]\+\).*/\1/')
-if [ -z "$MOUSE" ]; then
-  xinput set-button-map $MOUSE 1 0 3
-fi
+# which xset >/dev/null && xset m 3/2 0
+# MOUSE=$(xinput list | grep "Logitech USB Trackball" | sed 's/.*id=\([[:digit:]]\+\).*/\1/')
+# if [ -z "$MOUSE" ]; then
+#   xinput set-button-map $MOUSE 1 0 3
+# fi
