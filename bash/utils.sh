247ecc7771a2ecd0b6a76d53263ae51745bff6d0
Kevin Gisi <kevin@kevingisi.com>
2016-12-06 14:12:22 -0600

    Automatic update

--- bash/utils.sh
+++ bash/utils.sh
@@ -1,14 +1,13 @@
 function browser() {
-  if hash open 2>/dev/null; then
+  if hash xdg-open 2>/dev/null; then
     open $1;
-  elif hash xdg-open 2>/dev/null; then
-    xdg-open $1;
+  elif hash open 2>/dev/null; then
+    open $1;
   fi
 }
 
 function gh() {
   if [ -z $1 ]; then
-    echo https://github.com/$(git remote show origin | grep "Fetch URL" | sed 's/.*github.com:\(.*\)\.git/\1/')
     browser https://github.com/$(git remote show origin | grep "Fetch URL" | sed 's/.*github.com:\(.*\)\.git/\1/')
   else
     browser https://github.com/$1
