78214118fa9ab2a6ee702275b3765ae1b7672124
Kevin Gisi <kevin@kevingisi.com>
2017-06-13 07:17:36 -0500

    Automatic update

--- bash/encrypted_files.sh
+++ bash/encrypted_files.sh
@@ -1,7 +1,7 @@
 # Take a filename, create filename.enc
 function encrypt() {
   if [ ! -e "$HOME/.ssh/id_rsa.pub.pem" ]; then
-    openssl rsa -in $HOME/.ssh/id_rsa -pubout > $HOME/.ssh/id_rsa.pub.pem
+    openssl rsa -in $HOME/.ssh/id_rsa -pubout > $HOME/.ssh/id_rsa.pub.pem 2>/dev/null
   fi
 }
 
