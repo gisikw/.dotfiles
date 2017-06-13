8c9cbde3c6bb53ba69cd81bed01bbf901b601be0
Kevin Gisi <kevin@kevingisi.com>
2017-06-13 07:16:51 -0500

    Automatic update

--- bash/encrypted_files.sh
+++ bash/encrypted_files.sh
@@ -1,3 +1,11 @@
-function foobar() {
-  echo "Yep, this got sourced"
+# Take a filename, create filename.enc
+function encrypt() {
+  if [ ! -e "$HOME/.ssh/id_rsa.pub.pem" ]; then
+    openssl rsa -in $HOME/.ssh/id_rsa -pubout > $HOME/.ssh/id_rsa.pub.pem
+  fi
+}
+
+# Take a filename.env, create filename
+function decrypt() {
+
 }
