dad1c1b626f0f1126d9c17a2f73eaa171acb4af4
Kevin Gisi <kevin@kevingisi.com>
2017-06-13 07:23:25 -0500

    Automatic update

--- bash/encrypted_files.sh
+++ bash/encrypted_files.sh
@@ -3,9 +3,10 @@ function encrypt() {
   if [ ! -e "$HOME/.ssh/id_rsa.pub.pem" ]; then
     openssl rsa -in $HOME/.ssh/id_rsa -pubout > $HOME/.ssh/id_rsa.pub.pem 2>/dev/null
   fi
+  cat $1 | openssl rsautl -encrypt -pubin -inkey $HOME/.ssh/id_rsa.pub.pem > $1.enc
 }
 
 # Take a filename.env, create filename
 function decrypt() {
-
+  cat $1 | openssl rsautl -decrypt -inkey ~/.ssh/id_rsa > $(echo $1 | sed 's/\.[^.]*$//')
 }
