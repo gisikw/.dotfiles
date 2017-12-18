31ce3ad45f62e54b82c0015e6eafb96b7763e01b
Kevin Gisi <kevin@kevingisi.com>
2017-12-18 00:12:48 -0600

    Automatic update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+TARGET=~/.dotfiles
+
 if [ ! -d ~/.dotfiles ]; then
   which git > /dev/null || {
     echo "You must have git installed to use these dotfiles";  exit 1;
   }
-  git clone git@github.com:gisikw/.dotfiles.git ~/.dotfiles &&
+  (git clone git@github.com:gisikw/.dotfiles.git $TARGET|| git clone https://github.com/gisikw/.dotfiles.git $TARGET) &&
   if [ -f ~/.bash_profile ]; then
     echo "source ~/.dotfiles/bashrc" >> ~/.bash_profile
   else
