f0cdc6dd197764f20f32a0f386dca3a324f31534
Kevin Gisi <kevin@kevingisi.com>
2017-12-18 00:17:14 -0600

    Automatic update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
 TARGET=~/.dotfiles
+REPO=git@github.com:gisikw/.dotfiles.git
+SCRIPT="source ~/.dotfiles/bashrc"
 
 if [ ! -d ~/.dotfiles ]; then
   which git > /dev/null || {
     echo "You must have git installed to use these dotfiles";  exit 1;
   }
-  (git clone git@github.com:gisikw/.dotfiles.git $TARGET|| git clone https://github.com/gisikw/.dotfiles.git $TARGET) &&
-  if [ -f ~/.bash_profile ]; then
-    echo "source ~/.dotfiles/bashrc" >> ~/.bash_profile
-  else
-    echo "source ~/.dotfiles/bashrc" >> ~/.bashrc
-  fi
-  echo "Congratulations, the dotfiles have been installed. Please restart your shell."
+  git clone $REPO $TARGET && {
+    if [ -f ~/.bash_profile ]; then
+      echo $SCRIPT >> ~/.bash_profile
+    else
+      echo $SCRIPT >> ~/.bashrc
+    fi
+    echo "Congratulations, the dotfiles have been installed. Please restart your shell."
+  } || {
+    echo "Unable to access dotfiles repository. Could you be missing SSH keys?"; exit 1;
+  }
+else
+  echo "The dotfiles are already installed."
+  echo "Remove ~/.dotfiles and run this script again if you wish to reinstall."
 fi
