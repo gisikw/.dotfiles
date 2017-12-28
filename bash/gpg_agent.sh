831af3b3995415d0767cea942e1b2ea9038dba80
Kevin Gisi <kevin@kevingisi.com>
2017-12-28 17:55:54 +0000

    Automatic update

--- bash/gpg_agent.sh
+++ bash/gpg_agent.sh
@@ -1,8 +1,3 @@
-if test -f $HOME/.gpg-agent-info && \
-    kill -0 `cut -d: -f 2 $HOME/.gpg-agent-info` 2>/dev/null; then
-    GPG_AGENT_INFO=`cat $HOME/.gpg-agent-info | cut -c 16-`
-else
-    eval `gpg-agent --daemon --no-grab $HOME/.gpg-agent-info`
-fi
+eval `gpg-agent --daemon --no-grab` 2>/dev/null
 export GPG_TTY=`tty`
-export GPG_AGENT_INFO
+export GPG_AGENT_INFO=${HOME}/.gnupg/S.gpg-agent:0:1
