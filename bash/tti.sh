756167a56fd32c329992d61789ee3c361b04ab68
Kevin Gisi <kevin@kevingisi.com>
2018-02-02 13:28:11 -0600

    Automatic update

---
+++ bash/tti.sh
@@ -0,0 +1,11 @@
+function fids() {
+  RAILS_ENV=test RUBYOPT=W0 REDIS_URL="redis://localhost:6379/1" bundle exec rake db:init && bundle exec rake test:units && bundle exec rspec spec && bundle exec ruby test/integration/admin/admin_all.rb
+}
+
+function ids() {
+  RAILS_ENV=test RUBYOPT=W0 REDIS_URL="redis://localhost:6379/1" bundle exec rake db:init && bundle exec rake test:units && bundle exec rspec spec && bundle exec ruby test/integration/admin/admin_all.rb && bundle exec cucumber features
+}
+
+function verify() {
+  RAILS_ENV=test echo $RAILS_ENV
+}
