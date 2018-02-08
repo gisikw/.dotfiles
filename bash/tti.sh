bf491a0bfd7542180e5fe7d4ee56bf9ce81a1627
Kevin Gisi <kevin@kevingisi.com>
2018-02-08 13:16:32 -0600

    Automatic update

--- bash/tti.sh
+++ bash/tti.sh
@@ -23,9 +23,9 @@ function rids() {
   export RAILS_ENV=test 
   export RUBYOPT=W0 
   export REDIS_URL="redis://vger:6379/1" 
-  # bundle exec rake db:init && 
-  # bundle exec rake test:units && 
-  # bundle exec rspec spec && 
+  bundle exec rake db:init && 
+  bundle exec rake test:units && 
+  bundle exec rspec spec && 
   bundle exec ruby test/integration/admin/admin_all.rb && 
   bundle exec cucumber features
 }
