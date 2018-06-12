93d8788ac216c0d57da03ca05644e2096efcb875
Kevin Gisi <kevin@kevingisi.com>
2018-06-12 14:18:59 -0500

    Automatic update

--- bash/lazy_js.sh
+++ bash/lazy_js.sh
@@ -1,5 +1,5 @@
 function es6() {
   in=$1
   out=$2
-  echo $in | entr -s "NODE_PATH=$(npm -g root) npx babel --no-babelrc --presets=es2015,react $in | npx prettier --parser babylon > $out"
+  echo $in | entr -s "NODE_PATH=$(npm -g root) npx babel --no-babelrc --presets=es2015,react --plugins=transform-class-properties $in | npx prettier --parser babylon > $out"
 }
