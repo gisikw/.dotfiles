0cb9a0d0a15f4a01c19c1e6ae5b41d309c0ee8b1
Kevin Gisi <kevin@kevingisi.com>
2021-02-07 23:08:04 +0000

    Automatic update

--- bash/craml.sh
+++ bash/craml.sh
@@ -1,14 +1,12 @@
 # CRappy yAML Parser
 
 function craml_all() {
-  # FIXME: The awk objects to "\ " in contemporary versions of Ubuntu. Use yq
-  awk "/$2:/{p=1;next;print;}p&&/^[^\ ]/{p=0};p" $1 | awk '{ print $1 }' |
+  awk "/$2:/{p=1;next;print;}p&&/^[^\s]/{p=0};p" $1 | awk '{ print $1 }' |
   sed 's/:$//'
 }
 
 function craml_value() {
-  # FIXME: The awk objects to "\ " in contemporary versions of Ubuntu. Use yq
-  awk "/$2:/{p=1;next;print;}p&&/^[^\ ]/{p=0};p" $1 |
+  awk "/$2:/{p=1;next;print;}p&&/^[^\s]/{p=0};p" $1 |
   grep "^\s*$3:" |
   awk '{ print substr($0, index($0, $2)) }'
 }
