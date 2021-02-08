2d4cb98097d5e4940e297587e87097534de2e9a7
Kevin Gisi <kevin@kevingisi.com>
2021-02-08 00:09:04 +0000

    Automatic update

--- bash/craml.sh
+++ bash/craml.sh
@@ -1,12 +1,12 @@
 # CRappy yAML Parser
 
 function craml_all() {
-  awk "/$2:/{p=1;next;print;}p&&/^[^\ ]/{p=0};p" $1 | awk '{ print $1 }' |
+  awk "/$2:/{p=1;next;print;}p&&/^[^\ ]/{p=0};p" $1 2>/dev/null | awk '{ print $1 }' |
   sed 's/:$//'
 }
 
 function craml_value() {
-  awk "/$2:/{p=1;next;print;}p&&/^[^\ ]/{p=0};p" $1 |
+  awk "/$2:/{p=1;next;print;}p&&/^[^\ ]/{p=0};p" $1 2>/dev/null |
   grep "^\s*$3:" |
   awk '{ print substr($0, index($0, $2)) }'
 }
