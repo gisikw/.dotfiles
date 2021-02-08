7458e43b11ebd142471027ede07d2d9cd90eac37
Kevin Gisi <kevin@kevingisi.com>
2021-02-08 00:11:04 +0000

    Automatic update

--- bash/craml.sh
+++ bash/craml.sh
@@ -1,5 +1,10 @@
 # CRappy yAML Parser
 
+# NOTE: Later versions of awk complain about "\ " as invalid, but it doesn't
+# seem to affect behavior. Error messages have been suppressed. yq might be a
+# viable alternative, but installation via snap on Ubuntu has some file access
+# issues.
+
 function craml_all() {
   awk "/$2:/{p=1;next;print;}p&&/^[^\ ]/{p=0};p" $1 2>/dev/null | awk '{ print $1 }' |
   sed 's/:$//'
