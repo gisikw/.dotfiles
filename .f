29fb539dc73fee0c1d62c882fce54bb49f979f73
Kevin Gisi <gisikw@Kevins-MacBook-Air.local>
2023-05-01 15:55:35 -0500

    Installer

--- .f
+++ .f
@@ -3,7 +3,34 @@ _f_edit() {
 }
 
 _f_install() {
-  echo "Running 'install' command..."
+    command_exists() {
+        which "$1" > /dev/null 2>&1
+    }
+
+    missing_dependencies=()
+
+    command_exists git || missing_dependencies+=("git")
+    command_exists yq || missing_dependencies+=("yq")
+
+    if [ ${#missing_dependencies[@]} -gt 0 ]; then
+      echo "Installing missing dependencies: ${missing_dependencies[*]}"
+
+      if command_exists brew; then
+          brew install "${missing_dependencies[@]}"
+      elif command_exists apt; then
+          sudo apt update && sudo apt install -y "${missing_dependencies[@]}"
+      else
+          echo "Error: Neither apt nor brew is available. Please install the following missing dependencies manually: ${missing_dependencies[*]}"
+          return 1
+      fi
+    fi
+
+    git clone --quiet git@github.com:gisikw/.dotfiles.git $HOME/.dotfiles >/dev/null 2>&1
+    [ -f $HOME/.bash_profile ] && echo 'source $HOME/.dotfiles/.f' >> $HOME/.bash_profile
+    [ -f $HOME/.bashrc ] && echo 'source $HOME/.dotfiles/.f' >> $HOME/.bashrc
+    [ -f $HOME/.zshrc ] && echo 'source $HOME/.dotfiles/.f' >> $HOME/.zshrc
+
+    echo "Installation complete. Restart your shell or run . ~/.dotfiles/.f"
 }
 
 _f_uninstall() {
