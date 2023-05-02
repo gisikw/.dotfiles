f8012ec34fc5eaf9b7ea8cb8fe17a61d90cff00b
Kevin Gisi <gisikw@Kevins-MacBook-Air.local>
2023-05-02 10:07:32 -0500

    Automatic update

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,29 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    dependencies = {
+     "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("nvim-tree").setup {}
+    end,
+  }
+})
+
 vim.opt.syntax = "on"
 vim.opt.relativenumber = true
 vim.opt.number = true
