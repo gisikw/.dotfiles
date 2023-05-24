1c876d0c6463131391cd2362d5d1fa75698d85cd
Kevin Gisi <kevin.gisi@calendly.com>
2023-05-24 10:31:38 -0500

    Automatic update

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,12 +1,14 @@
-local function my_on_attach(bufnr) {
+local function my_on_attach(bufnr)
   local api = require('nvim-tree.api')
 
   local function opts(desc)
     return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
   end
 
-  vim.keymap.set('n', '-', api.node.open.vertical, opts('Open: Vertical Split'))
-}
+  api.config.mappings.default_on_attach(bufnr)
+  vim.keymap.set('n', '|', api.node.open.vertical, opts('Open: Vertical Split'))
+  vim.keymap.set('n', '-', api.node.open.horizontal, opts('Open: Horizontal Split'))
+end
 
 return {
   "nvim-tree/nvim-tree.lua",
@@ -15,6 +17,8 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
-    require("nvim-tree").setup {}
+    require("nvim-tree").setup {
+      on_attach = my_on_attach
+    }
   end,
 }
