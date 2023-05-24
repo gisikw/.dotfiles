f9f3178c2a321826fbd986f7c71a06563e6fda11
Kevin Gisi <kevin.gisi@calendly.com>
2023-05-24 10:27:53 -0500

    Automatic update

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,3 +1,13 @@
+local function my_on_attach(bufnr) {
+  local api = require('nvim-tree.api')
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  vim.keymap.set('n', '-', api.node.open.vertical, opts('Open: Vertical Split'))
+}
+
 return {
   "nvim-tree/nvim-tree.lua",
   version = "*",
