e62790bfca2a75c1a2c07acad42966b567e2b9e0
Kevin Gisi <kevin.gisi@calendly.com>
2023-05-24 10:22:08 -0500

    Automatic update

--- nvim/after/plugin/nvim-tree.lua
+++ nvim/after/plugin/nvim-tree.lua
@@ -1,2 +1,2 @@
 vim.api.nvim_set_keymap('n', '<leader>d', ':NvimTreeToggle<cr>', {})
-vim.api.nvim_set_keymap('n', '<leader>f', ':NvimFindFile<cr>', {})
+vim.api.nvim_set_keymap('n', '<leader>f', ':NvimTreeFindFile<cr>', {})
