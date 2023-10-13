8e3b25289b0881d928d54eaf709ea08fc2559ead
Kevin Gisi <kevin.gisi@calendly.com>
2023-10-13 10:43:57 -0500

    Automatic update

--- .f
+++ .f
@@ -1,114 +1,116 @@
 #!/bin/bash
-
+dotfiles_bin="$HOME/.dotfiles/bin"
 shell_rcs=(.bashrc .bash_profile .zshrc)
 
 _f_edit() {
-	(cd $HOME/.dotfiles && nvim -c "NvimTreeToggle" config.yml -c "wincmd w" && .f)
+  (cd $HOME/.dotfiles && nvim -c "NvimTreeToggle" config.yml -c "wincmd w" && .f)
 }
 
 _f_install() {
-	command_exists() {
-		which "$1" >/dev/null 2>&1
-	}
+  command_exists() { which "$1" >/dev/null 2>&1 }
+  missing_dependencies=()
 
-	missing_dependencies=()
+  command_exists git || missing_dependencies+=("git")
+  command_exists yq || missing_dependencies+=("yq")
 
-	command_exists git || missing_dependencies+=("git")
-	command_exists yq || missing_dependencies+=("yq")
+  if [ ${#missing_dependencies[@]} -gt 0 ]; then
+    echo "Installing missing dependencies: ${missing_dependencies[*]}"
+    if command_exists brew; then
+      brew install "${missing_dependencies[@]}"
+    elif command_exists apt; then
+      sudo apt update && sudo apt install -y "${missing_dependencies[@]}"
+    else
+      echo "Error: Neither apt nor brew is available. Please install the following missing dependencies manually: ${missing_dependencies[*]}"
+      return 1
+    fi
+  fi
 
-	if [ ${#missing_dependencies[@]} -gt 0 ]; then
-		echo "Installing missing dependencies: ${missing_dependencies[*]}"
-
-		if command_exists brew; then
-			brew install "${missing_dependencies[@]}"
-		elif command_exists apt; then
-			sudo apt update && sudo apt install -y "${missing_dependencies[@]}"
-		else
-			echo "Error: Neither apt nor brew is available. Please install the following missing dependencies manually: ${missing_dependencies[*]}"
-			return 1
-		fi
-	fi
-
-	git clone --quiet git@github.com:gisikw/.dotfiles.git $HOME/.dotfiles >/dev/null 2>&1
-        for rc_file in "${shell_rcs[@]}"; do
-          [ -f "$HOME/$rc_file" ] && echo 'source $HOME/.dotfiles/.f' >> $HOME/$rc_file
-        done
-	echo "Installation complete. Please restart your shell or run . ~/.dotfiles/.f"
+  git clone --quiet git@github.com:gisikw/.dotfiles.git $HOME/.dotfiles >/dev/null 2>&1
+  for rc_file in "${shell_rcs[@]}"; do
+    [ -f "$HOME/$rc_file" ] && echo 'source $HOME/.dotfiles/.f' >> $HOME/$rc_file
+  done
+  echo "Installation complete. Please restart your shell or run . ~/.dotfiles/.f"
 }
 
 _f_uninstall() {
-	while read symlink; do
-		rm -rf $symlink
-	done < <(yq '.symlinks | keys' ~/.dotfiles/config.yml | sed 's/- //')
-	rm -rf $HOME/.dotfiles
-        for rc_file in "${shell_rcs[@]}"; do
-          if [[ "$(uname)" == "Darwin" ]]; then
-            sed -i '' '/source \$HOME\/.dotfiles\/.f/d' $HOME/$rc_file 2>/dev/null
-          else
-            sed -i '/source \$HOME\/.dotfiles\/.f/d' $HOME/$rc_file 2>/dev/null
-          fi
-        done
-	echo "Dotfiles uninstalled. Please restart your shell."
+  while read symlink; do
+    rm -rf $symlink
+  done < <(yq '.symlinks | keys' ~/.dotfiles/config.yml | sed 's/- //')
+  rm -rf $HOME/.dotfiles
+  for rc_file in "${shell_rcs[@]}"; do
+    if [[ "$(uname)" == "Darwin" ]]; then
+      sed -i '' '/source \$HOME\/.dotfiles\/.f/d' $HOME/$rc_file 2>/dev/null
+    else
+      sed -i '/source \$HOME\/.dotfiles\/.f/d' $HOME/$rc_file 2>/dev/null
+    fi
+  done
+  echo "Dotfiles uninstalled. Please restart your shell."
 }
 
 _f_main() {
-	function github_available() {
-		ping -c 1 -W 1 github.com >/dev/null 2>&1
-	}
-	function commit_dotfile_changes() {
-		(
-			cd $HOME/.dotfiles
-			if ! test -z "$(git status --porcelain)"; then
-				git add --all >/dev/null 2>&1
-				git commit -m "Automatic update" >/dev/null 2>&1
-			fi
-		)
-	}
-	function update_dotfile_repository() {
-		(
-			cd ~/.dotfiles
-			git pull origin main >/dev/null 2>&1
-			git push origin main >/dev/null 2>&1 &
-		)
-	}
-	if github_available; then
-		commit_dotfile_changes
-		update_dotfile_repository
-	fi
+  function github_available() {
+    ping -c 1 -W 1 github.com >/dev/null 2>&1
+  }
+
+  function commit_dotfile_changes() {
+    (
+      cd $HOME/.dotfiles
+      if ! test -z "$(git status --porcelain)"; then
+        git add --all >/dev/null 2>&1
+        git commit -m "Automatic update" >/dev/null 2>&1
+      fi
+    )
+  }
+
+  function update_dotfile_repository() {
+    (
+      cd ~/.dotfiles
+      git pull origin main >/dev/null 2>&1
+      git push origin main >/dev/null 2>&1 &
+    )
+  }
+
+  if github_available; then
+    commit_dotfile_changes
+    update_dotfile_repository
+  fi
 }
 
 _f_show_usage() {
-	echo "Usage: .f [edit|install|uninstall]"
+  echo "Usage: .f [edit|install|uninstall]"
 }
 
 .f() {
-	local command="$1"
+  local command="$1"
 
-	case "$command" in
-	edit)
-		_f_edit
-		;;
-	install)
-		_f_install
-		;;
-	uninstall)
-		_f_uninstall
-		;;
-	*)
-		if [ -n "$1" ]; then
-			_f_show_usage
-		else
-			_f_main
-		fi
-		;;
-	esac
+  case "$command" in
+  edit)
+    _f_edit
+    ;;
+  install)
+    _f_install
+    ;;
+  uninstall)
+    _f_uninstall
+    ;;
+  *)
+    if [ -n "$1" ]; then
+      _f_show_usage
+    else
+      _f_main
+    fi
+    ;;
+  esac
 }
 
 if [[ $ZSH_NAME ]]; then
-	current_file_path="${(%):-%x}"
+  current_file_path="${(%):-%x}"
 else
-	current_file_path="${BASH_SOURCE[0]}"
+  current_file_path="${BASH_SOURCE[0]}"
 fi
 if [ "$(realpath $current_file_path)" = "$HOME/.dotfiles/.f" ]; then
-	source $HOME/.dotfiles/init.sh
+  source $HOME/.dotfiles/init.sh
+  if [[ ":$PATH:" != *":$dotfiles_bin:"* ]]; then
+    export PATH="$PATH:$dotfiles_bin"
+  fi
 fi
