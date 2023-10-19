ed9f76a7449ecd1ff4858e014c6cf5cc491bc01b
Kevin Gisi <kevin.gisi@calendly.com>
2023-10-19 10:17:58 -0500

    Automatic update

--- nvim/init.lua
+++ nvim/init.lua
@@ -76,3 +76,57 @@ end
 
 vim.api.nvim_set_keymap('i', '<Tab>', 'v:lua.insert_tab_wrapper()', {expr = true, noremap = true})
 vim.api.nvim_set_keymap('i', '<S-Tab>', '<C-n>', {noremap = true})
+
+_G.TermSplitCmd = function(cmd)
+    local buf_name = "special_terminal"
+    local existing_buf = vim.fn.bufnr(buf_name)
+    local terminal_win = nil
+
+    -- Check if our buffer is currently visible in any window.
+    for _, win in ipairs(vim.api.nvim_list_wins()) do
+        if vim.api.nvim_win_get_buf(win) == existing_buf then
+            terminal_win = win
+            break
+        end
+    end
+
+    -- If the terminal buffer is visible in another window, close it.
+    if terminal_win then
+        vim.api.nvim_win_close(terminal_win, false)
+        vim.cmd(string.format("bdelete %d", existing_buf))
+    end
+
+    -- Always create a new terminal in a new split.
+    vim.cmd("belowright split")
+    vim.cmd(string.format("term %s", cmd))
+    vim.api.nvim_buf_set_name(0, buf_name)
+end
+
+
+vim.api.nvim_command('command! -nargs=* TermSplitCmd lua _G.TermSplitCmd(<q-args>)')
+
+_G.run_leader_cmd = function(num)
+  local cmd_var = "leader_" .. tostring(num)
+  if vim.g[cmd_var] == nil then
+    local success, user_input = pcall(vim.fn.input, "Temp cmd: ")
+
+    if not success or user_input == "" then return end
+    vim.g[cmd_var] = "TermSplitCmd " .. user_input:gsub('%%', vim.fn.expand('%'))
+  end
+  vim.cmd(vim.g[cmd_var])
+end
+
+_G.clear_leader_cmds = function()
+  for i = 1, 10 do
+    local cmd_var = "leader_" .. tostring(i)
+    vim.g[cmd_var] = nil
+  end
+  print("Cleared all temp commands")
+end
+
+for i = 1, 10 do
+  local key = i % 10
+  vim.api.nvim_set_keymap('n', '<leader>' .. tostring(key), string.format(':lua run_leader_cmd(%d)<CR>', i), { noremap = true, silent = true })
+end
+vim.api.nvim_set_keymap('n', '<leader>`', ':lua _G.clear_leader_cmds()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>q', ':bdelete<CR>', { noremap = true, silent = true })
