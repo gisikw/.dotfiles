717ae0684c51be947ba3fb9386af1fa2f4728bbd
Kevin Gisi <kevin.gisi@calendly.com>
2025-03-02 09:30:46 -0600

    Disable mouse cursor click

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -19,6 +19,8 @@ vim.opt.termguicolors = true
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
+vim.opt.mouse = ""
+
 -------------------------------------------------------------------------------
 -- KEYBINDINGS
 -------------------------------------------------------------------------------
